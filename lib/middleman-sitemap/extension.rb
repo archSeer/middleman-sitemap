e6bfb57dafd978c4d82619b36d52f195f9db0150
Blaž Hrastnik <blaz@mxxn.io>
2016-10-23 00:52:40 +0900

    Update build_dir path to work with middleman v4

--- lib/middleman-sitemap/extension.rb
+++ lib/middleman-sitemap/extension.rb
@@ -43,12 +43,12 @@ class Sitemap < ::Middleman::Extension
     template = Tilt::ERBTemplate.new(File.expand_path(File.join("#{File.dirname(__FILE__)}", "../../templates/sitemapindex.xml.erb")))
     sitemap = template.render(self)
 
-    outfile = File.join(app.build_dir, "sitemap.xml")
+    outfile = File.join(app.config[:build_dir], "sitemap.xml")
     File.open(outfile, 'w') {|f| f.write(sitemap) }
 
-    @builder.say_status :create, "#{app.build_dir}/sitemap.xml"
+    @builder.say_status :create, "#{app.config[:build_dir]}/sitemap.xml"
 
-    return "#{app.build_dir}/sitemap.xml"
+    return "#{app.config[:build_dir]}/sitemap.xml"
   end
 
   def build_sitemap(name, pages)
@@ -58,12 +58,12 @@ class Sitemap < ::Middleman::Extension
     template = Tilt::ERBTemplate.new(templates_path, 0, :trim => '>')
     sitemap = template.render(self)
 
-    outfile = File.join(app.build_dir, name)
+    outfile = File.join(app.config[:build_dir], name)
     File.open(outfile, 'w') {|f| f.write(sitemap) }
 
-    @builder.say_status :create, "#{app.build_dir}/#{name}"
+    @builder.say_status :create, "#{app.config[:build_dir]}/#{name}"
 
-    return "#{app.build_dir}/#{name}"
+    return "#{app.config[:build_dir]}/#{name}"
   end
 
   def build_multiple_sitemaps(pages)
